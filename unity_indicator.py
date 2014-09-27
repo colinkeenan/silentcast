d748baa04575388a5d9757bef0194a14044d8f07
Seth-Johnson <sethj0438@gmail.com>
2014-09-26 19:33:10 -0700

    Added an indicator for unity since yad doesn't work.

---
+++ unity_indicator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from gi.repository import Gtk
+from gi.repository import AppIndicator3 as appindicator
+
+class IndicatorSilentcast:
+    def __init__(self):
+        self.indicator = appindicator.Indicator.new ("silentcast", "/usr/share/doc/silentcast/gnome-stop.png", appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.indicator.set_status (appindicator.IndicatorStatus.ACTIVE)
+        
+        self.menu = Gtk.Menu()
+        
+        doneItem = Gtk.MenuItem("Done")
+        doneItem.connect("activate", self.finishRecording)
+        doneItem.show()
+        self.menu.append(doneItem)
+        
+        self.menu.show()
+        self.indicator.set_menu(self.menu)
+        
+    def finishRecording(self, widget):
+            Gtk.main_quit()
+            
+def main():
+    Gtk.main()
+    exit(0)
+
+if __name__ == "__main__":
+    indicator = IndicatorSilentcast()
+    main()
